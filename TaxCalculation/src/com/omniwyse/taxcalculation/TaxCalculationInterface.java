a4efca4c00d8ba6c4cd71b573fa400d27a597317
Kalyan9493 <57888686+Kalyan9493@users.noreply.github.com>
2020-01-25 00:13:46 +0530

    Tax Calculation based on CTC

---
+++ TaxCalculation/src/com/omniwyse/taxcalculation/TaxCalculationInterface.java
@@ -0,0 +1,11 @@
+package com.omniwyse.taxcalculation;
+
+public interface TaxCalculationInterface {
+	public void esiCalculation(long ctc);
+	public void pfCalculation(long ctc);
+	public void ptCalculation(long ctc);
+	public void incometaxCalculation(long ctc);
+	public void rebate(long ctc);
+	public void empNetSalary(long ctc);
+
+}
