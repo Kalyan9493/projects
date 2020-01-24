c07ad8e8494a43c7f3006f4f27a3f28f1ca8ad73
Kalyan9493 <57888686+Kalyan9493@users.noreply.github.com>
2020-01-24 23:45:10 +0530

    Add files via upload

---
+++ atmWorking/ATMProcedure.java
@@ -0,0 +1,9 @@
+package atmWorking;
+
+public interface ATMProcedure {
+	void showBalance(int amount);
+	int withdrawAmount(int amount);
+	void showRemainingBalance(int amount);
+	
+
+}
---
+++ atmWorking/Operations.java
@@ -0,0 +1,35 @@
+package atmWorking;
+
+import java.util.Scanner;
+
+public class Operations implements ATMProcedure{
+
+	private Scanner s;
+	@Override
+	public void showBalance(int amount) {
+		System.out.println("Your Account Balance is:"+amount);
+	}
+
+	public int withdrawAmount(int amount) {
+		System.out.println("Enter The Amount You Want To Withdraw:");
+		s = new Scanner(System.in);
+		int withdrawamount=s.nextInt();
+		if(withdrawamount>amount) {
+			System.out.println("InSufficient Balance");
+		}
+		else {
+			amount=amount-withdrawamount;
+			System.out.println("Remaining Balance Is:"+amount);
+		}
+		return amount;
+		
+	}
+
+	@Override
+	public void showRemainingBalance(int amount) {
+		
+		System.out.println("Remaining Balance Is:"+amount);
+		
+	}
+
+}
