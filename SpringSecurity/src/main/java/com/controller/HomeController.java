8c5161af9feee14e9fd39fb667459d2769fb66f2
Kalyan9493 <57888686+Kalyan9493@users.noreply.github.com>
2020-03-21 16:07:03 +0530

    spring security added

---
+++ SpringSecurity/src/main/java/com/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home.jsp";
+	}
+
+	@RequestMapping("/login")
+	public String login() {
+		return "login.jsp";
+	}
+	@RequestMapping("/logout")
+	public String logout() {
+		return "logout.jsp";
+	}
+	
+	@RequestMapping("/logout-success")
+	public String logoutSuccess() {
+		return "login.jsp";
+	}
+	
+}
